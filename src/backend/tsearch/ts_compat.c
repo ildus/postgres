a5e25ecb507e84b73f108c53061b9b56f1cdd6f5
Ildus K <i.kurbangaliev@gmail.com>
2017-05-24 15:24:07 +0300

    Add forgotten files

---
+++ src/backend/tsearch/ts_compat.c
@@ -0,0 +1,82 @@
+#include "postgres.h"
+#include "tsearch/ts_type.h"
+
+/*
+ * Defition of old WordEntry struct in TSVector. Because of limitations
+ * in size (max 1MB for lexemes), a format have changed
+ */
+typedef struct
+{
+	uint32
+				haspos:1,
+				len:11,
+				pos:20;
+} OldWordEntry;
+
+typedef struct
+{
+	uint16		npos;
+	WordEntryPos pos[FLEXIBLE_ARRAY_MEMBER];
+} OldWordEntryPosVector;
+
+#define _OLDPOSVECPTR(x, e)	\
+	((OldWordEntryPosVector *)(STRPTR(x) + SHORTALIGN((e)->pos + (e)->len)))
+#define OLDPOSDATALEN(x,e) ( ( (e)->haspos ) ? (_OLDPOSVECPTR(x,e)->npos) : 0 )
+#define OLDPOSDATAPTR(x,e) (_OLDPOSVECPTR(x,e)->pos)
+
+TSVector
+tsvector_upgrade(Datum orig)
+{
+	bool       writable;
+	WordEntry *newentry;
+	int	       i,
+	           offset;
+	TSVector   vec = (TSVector) PG_DETOAST_DATUM(orig);
+
+	/* If already in new format, return as is */
+	if (vec->size_ & TS_FLAG_EXPANDED)
+		return vec;
+
+	/* We got an old format, converting to the new */
+	writable = ((void *) vec != (void *) DatumGetPointer(orig));
+	if (!writable)
+		vec = (TSVector) PG_DETOAST_DATUM_COPY(orig);
+
+	/* Contains current position in new tsvector */
+	offset = 0;
+
+	for (i = 0; i < vec->size_; i++)
+	{
+		/* Save old values */
+		OldWordEntry entry = *((OldWordEntry *)(vec->entries + i));
+
+		/* Fill new fields */
+		newentry = vec->entries + i;
+		newentry->len = entry.len;
+		newentry->npos = OLDPOSDATALEN(vec, &entry);
+		newentry->_unused = 0;
+
+		/* Move lexeme */
+		memmove(STRPTR(vec) + offset, STRPTR(vec) + entry.pos, entry.len);
+		offset += entry.len;
+
+		/* Move positions */
+		if (entry.haspos)
+		{
+			offset = SHORTALIGN(offset);
+			memmove(STRPTR(vec) + offset, OLDPOSDATAPTR(vec, &entry),
+				sizeof(WordEntryPos) * newentry->npos);
+			offset += sizeof(WordEntryPos) * newentry->npos;
+		}
+	}
+
+	/* New size will be less than old size if tsvector had positions */
+	SET_VARSIZE(vec, CALCDATASIZE(vec->size_, offset));
+
+	/*
+	 * Mark TSVector as new, after this operation vec->size will
+	 *	contain special flag
+	 */
+	TS_SETCOUNT(vec, vec->size_);
+	return vec;
+}
---
+++ src/backend/tsearch/ts_expand.c
@@ -0,0 +1,166 @@
+/*-------------------------------------------------------------------------
+ *
+ * ts_expand.c
+ *	  Expanded tsvector functions
+ *
+ * Portions Copyright (c) 1996-2015, PostgreSQL Global Development Group
+ *
+ *
+ * IDENTIFICATION
+ *	  src/backend/tsearch/ts_expand.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+#include "tsearch/ts_utils.h"
+
+#define EV_MAGIC 0xF4F4F4F4
+
+static Size EV_get_flat_size(ExpandedObjectHeader *eohptr);
+static void EV_flatten_into(ExpandedObjectHeader *eohptr,
+	void *result, Size allocated_size);
+
+static const ExpandedObjectMethods EV_methods =
+{
+	EV_get_flat_size,
+	EV_flatten_into
+};
+
+static void
+EV_flatten_into(ExpandedObjectHeader *eohptr, void *result,
+	Size allocated_size)
+{
+	ExpandedTSVectorHeader *evhptr = (ExpandedTSVectorHeader *)eohptr;
+	memcpy(result, PG_DETOAST_DATUM(evhptr->datum), allocated_size);
+}
+
+static Size
+EV_get_flat_size(ExpandedObjectHeader *eohptr)
+{
+	ExpandedTSVectorHeader *evhptr = (ExpandedTSVectorHeader *)eohptr;
+	return VARSIZE(evhptr->datum);
+}
+
+
+/*
+ * expand_tsvector - converts tsvector datum to an expanded
+ * tsvector
+ */
+static Datum
+expand_tsvector(Datum vectorDatum, MemoryContext parentcontext)
+{
+	MemoryContext objcxt, oldcxt;
+	ExpandedTSVectorHeader *evh;
+	TSVector vector;
+
+	/* If already expanded do nothing */
+	if (VARATT_IS_EXTERNAL_EXPANDED(DatumGetPointer(vectorDatum)))
+		return vectorDatum;
+
+	/* Allocate private context for expanded object */
+	objcxt = AllocSetContextCreate(parentcontext,
+								   "expanded tsvector",
+								   ALLOCSET_SMALL_MINSIZE,
+								   ALLOCSET_SMALL_INITSIZE,
+								   ALLOCSET_DEFAULT_MAXSIZE);
+
+	/* Set up expanded tsvector header */
+	evh = (ExpandedTSVectorHeader *)
+		MemoryContextAlloc(objcxt, sizeof(ExpandedTSVectorHeader));
+
+	EOH_init_header(&evh->hdr, &EV_methods, objcxt);
+
+	/* Get only size_ value of TSVector */
+	vector = (TSVector) PG_DETOAST_DATUM_SLICE(vectorDatum, 0, sizeof(int32));
+
+	oldcxt = MemoryContextSwitchTo(objcxt);
+	evh->ev_magic = EV_MAGIC;
+	evh->datum = vectorDatum;
+	evh->count = TS_COUNT(vector);
+	evh->positions = (uint32 *)palloc0(sizeof(uint32) * evh->count);
+	evh->entries = (WordEntry *) VARDATA(PG_DETOAST_DATUM_SLICE(vectorDatum,
+		sizeof(int32), sizeof(WordEntry) * evh->count));
+	evh->maxidx = 0;
+
+	MemoryContextSwitchTo(oldcxt);
+	pfree(vector);
+
+	/* return a R/O pointer to the expanded tsvector */
+	return EOHPGetRODatum(&evh->hdr);
+}
+
+/* Returns lexeme and its entry from expanded TSVector */
+char *
+tsvector_getlexeme(TSVectorExpanded vec, int idx, WordEntry **we)
+{
+	WordEntry	   *entry;
+	char		   *lexeme;
+	MemoryContext	oldcxt;
+	int32			pos,
+					pos2;
+
+	Assert(idx < vec->count);
+	Assert(we != NULL);
+	*we = &vec->entries[idx];
+
+	/* Calculate count for slice */
+	pos2 = pos = tsvector_getposition(vec, idx);
+	pos2 += (*we)->len;
+	if ((*we)->npos)
+	{
+		pos2 = SHORTALIGN(pos2);
+		pos2 += (*we)->npos * sizeof(WordEntryPos);
+	}
+
+	/* we do all memory allocations in the context of the expanded object */
+	oldcxt = MemoryContextSwitchTo(vec->hdr.eoh_context);
+	lexeme = (char *) VARDATA(PG_DETOAST_DATUM_SLICE(vec->datum,
+		sizeof(int32) + sizeof(WordEntry) * vec->count + pos,
+		pos2 - pos + 1));
+	MemoryContextSwitchTo(oldcxt);
+
+	return lexeme;
+}
+
+/* Returns position for index, and calculates positions cache if needed */
+int
+tsvector_getposition(TSVectorExpanded vec, int idx)
+{
+	WordEntry *entry;
+	int32 i, pos;
+
+	if (idx > vec->maxidx)
+	{
+		pos = vec->positions[vec->maxidx];
+		entry = &vec->entries[vec->maxidx];
+
+		for (i = vec->maxidx + 1; i <= idx; i++)
+		{
+			INCRPTR(entry, pos);
+			vec->positions[i] = pos;
+		}
+		vec->maxidx = idx;
+	}
+
+	/* Calculate count for slice */
+	return vec->positions[idx];
+}
+
+ExpandedTSVectorHeader *
+DatumGetExpandedTSVector(Datum d)
+{
+	if (VARATT_IS_EXTERNAL_EXPANDED(DatumGetPointer(d)))
+	{
+		ExpandedTSVectorHeader *evh =
+			(ExpandedTSVectorHeader *) DatumGetEOHP(d);
+
+		Assert(evh->ev_magic == EV_MAGIC);
+		return evh;
+	}
+
+	/* Else expand the hard way */
+	d = expand_tsvector(d, CurrentMemoryContext);
+	return (ExpandedTSVectorHeader *) DatumGetEOHP(d);
+}
+
